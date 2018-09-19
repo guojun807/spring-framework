0b13a068fe340ab446d6ffb08aeaa3247ba1005e
guojun807 <434996064@qq.com>
2018-09-20 00:16:48 +0800

    aop

---
+++ spring-beans/src/test/java/org/springframework/tests/guojun/BeanFactoryTest.java
@@ -0,0 +1,9 @@
+package org.springframework.tests.guojun;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+
+public class BeanFactoryTest {
+	public static void main(String[] args) {
+//		new XmlBeanFactory()
+	}
+}
